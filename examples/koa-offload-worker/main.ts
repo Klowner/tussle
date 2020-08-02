9f684bd65eca6f600390c8a8437ff86af8f1cc5d
Mark Riedesel <mark@klowner.com>
2020-08-02 06:32:18 -0500

    remove junk

--- examples/koa-offload-worker/main.ts
+++ examples/koa-offload-worker/main.ts
@@ -2,9 +2,9 @@ import { Upload } from 'tus-js-client';
 
 function addEventListeners(): void {
   const el = document.getElementById('file');
-  console.log('el', el);
   el.addEventListener('change', (e) => {
     const file = (e.target as HTMLInputElement).files[0];
+    console.log('sending', file.name, file.type);
     const upload = new Upload(file, {
       endpoint: 'http://localhost:8080/files',
       retryDelays: [0, 1000],
--- packages/tussle-core/lib/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var core_1 = require("./core");
-module.exports = core_1.Tussle;
