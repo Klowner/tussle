35caa6fd4dd8875ca827c69c1ea449a6434c11e1
Mark Riedesel <mark@klowner.com>
2020-09-02 13:33:01 -0500

    alter TTLCache to not use setInterval()

--- packages/core/src/util/ttlcache.ts
+++ packages/core/src/util/ttlcache.ts
@@ -5,22 +5,29 @@ interface TTLCacheItem<T> {
   data: T;
 }
 
+// Initially I just used a setInterval() to periodically perform
+// garbage collection, but some environments such as Cloudflare
+// don't allow certain API calls outside of request contexts.
+
+const isExpired = (now: number, lastAccess: number, ttl: number): boolean =>
+  now - lastAccess >= ttl;
+
 export class TTLCache<T> {
+  private lastGarbageCollection: number;
+
   constructor(
     private readonly ttl = 10 * 60 * 1000,
     private readonly garbageCollectionInterval = 5 * 60 * 1000,
     private readonly cache: Record<string, TTLCacheItem<T>> = {},
     private readonly now: () => number = () => Date.now(),
   ) {
-    setInterval(
-      () => this.garbageCollect(),
-      this.garbageCollectionInterval
-    );
+    this.lastGarbageCollection = this.now();
   }
 
   public async getOrCreate(key: string, create: () => Promise<T>): Promise<T> {
     const hit = this.cache[key];
     const now = this.now();
+    this.asyncGarbageCollect(now);
     if (!hit) {
       const created = await create();
       return (this.cache[key] = {
@@ -33,9 +40,11 @@ export class TTLCache<T> {
   }
 
   public getItem(key: string): T | null {
+    const now = this.now();
     const hit = this.cache[key];
+    this.asyncGarbageCollect(now);
     if (hit) {
-      hit.atime = this.now();
+      hit.atime = now;
       return hit.data;
     }
     return null;
@@ -45,9 +54,18 @@ export class TTLCache<T> {
     const now = this.now();
     const { ttl, cache } = this;
     for (const key in cache) {
-      if (now - cache[key].atime > ttl) {
+      if (isExpired(now, cache[key].atime, ttl)) {
         delete cache[key];
       }
+    }
+  }
+
+  private asyncGarbageCollect(now: number, force = false): void {
+    if (force || isExpired(now, this.lastGarbageCollection, this.garbageCollectionInterval)) {
+      this.lastGarbageCollection = now;
+      new Promise(() => {
+        this.garbageCollect();
+      });
     }
   }
 }
