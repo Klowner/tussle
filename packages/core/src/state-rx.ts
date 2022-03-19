89733d64a115cfdeee7a2096eb3d5045290c7c0c
Mark Riedesel <mark@klowner.com>
2022-03-19 09:34:52 -0500

    update state-rx

--- packages/core/src/state-rx.ts
+++ packages/core/src/state-rx.ts
@@ -1,10 +1,10 @@
 import {TussleStateService} from "@tussle/spec/interface/state";
-import type { Observable } from "rxjs";
-import { defer, from } from "rxjs";
+import {defer, from, Observable} from "rxjs";
 import {map} from "rxjs/operators";
 
-export class StateRX<U, T extends TussleStateService<U>> {
-  constructor (private readonly stateService: T) {}
+
+class StateRX<U> {
+  constructor (private readonly stateService: TussleStateService<U>) {}
 
   getItem(location: string): Observable<U | null> {
     return defer(() => from(this.stateService.getItem(location))).pipe(
@@ -14,13 +14,17 @@ export class StateRX<U, T extends TussleStateService<U>> {
 
   setItem(location: string, data: U): Observable<U>;
   setItem(location: string, data: null): Observable<null>;
-  setItem(location: string, value: U | null): Observable<U | null> {
-    if (value === null) {
+  setItem(location: string, data: U | null): Observable<U | null> {
+    if (data === null) {
       return from(this.stateService.removeItem(location));
     } else {
-      return from(this.stateService.setItem(location, value)).pipe(
-        map(() => value),
+      return from(this.stateService.setItem(location, data)).pipe(
+        map(() => data),
       );
     }
   }
 }
+
+export {
+  StateRX,
+};
