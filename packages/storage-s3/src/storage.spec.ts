0df42c8562d4c49afa9aafae83eb4ab2cb691931
Mark Riedesel <mark@klowner.com>
2022-04-05 13:46:44 -0400

    storage-s3: fix test

--- packages/storage-s3/src/storage.spec.ts
+++ packages/storage-s3/src/storage.spec.ts
@@ -1,22 +1,21 @@
 import { storageServiceTests } from '@tussle/spec';
 import { TussleStorageS3 } from './storage';
 import { TussleStateMemory } from '@tussle/state-memory';
-import { TussleRequestAxios } from '@tussle/request-axios';
-
 
 storageServiceTests(
   '@tussle/storage-s3',
   async () => new TussleStorageS3({
     s3: {
-      endpoint: 'https://s3.us-west-001.backblazeb2.com',
-      region: 'us-west-001',
-      credentials: {
-        accessKeyId: '<S3_ACCESS_KEY_ID>',
-        secretAccessKey: '<S3_SECRET_ACCESS_KEY>',
+      client: {
+        endpoint: 'https://s3.us-west-001.backblazeb2.com',
+        region: 'us-west-001',
+        credentials: {
+          accessKeyId: '<S3_ACCESS_KEY_ID>',
+          secretAccessKey: '<S3_SECRET_ACCESS_KEY>',
+        },
       },
+      bucket: '<S3_BUCKET>',
     },
-    s3bucket: '<S3_BUCKET>',
     stateService: new TussleStateMemory(),
-    requestService: new TussleRequestAxios(),
   }),
 );
