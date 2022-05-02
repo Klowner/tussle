50fffff0fcb3e8e59a33ae2ac8c10a870766729e
Mark Riedesel <mark@klowner.com>
2022-05-01 21:09:57 -0400

    remove default path from core creation handler (it may be set during storage selection)

--- packages/core/src/handlers/create.ts
+++ packages/core/src/handlers/create.ts
@@ -2,44 +2,20 @@ import type { TussleIncomingRequest } from '@tussle/spec/interface/request';
 import type { TussleStorageCreateFileResponse } from '@tussle/spec/interface/storage';
 import type { Tussle } from '../core';
 import { decode } from 'js-base64';
-import { MonoTypeOperatorFunction, Observable, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 
-function defaultPath(path: string, filename: string): string {
-  return [
-    path,
-    Math.floor(Math.random() * 1e16).toString(16),
-    encodeURIComponent(filename),
-  ].join('/');
-}
-
-// If the before-create hook didn't set the location, for the file being
-// created, then generate a default based on the request path and metadata
-// filename.
-const ensureFilePath = (originalPath: string): MonoTypeOperatorFunction<TussleCreationParams> =>
-  map(params => {
-    if (params.path === originalPath) {
-      params.path = defaultPath(params.path, params.uploadMetadata.filename);
-    }
-    return params;
-  });
-
-
-
 export default function handleCreate<R>(
-  core: Tussle,
+  _core: Tussle,
   ctx: Readonly<TussleIncomingRequest<R>>
 ): Observable<TussleIncomingRequest<R>> {
   const params = extractCreationHeaders(ctx);
-  const originalPath = params.path;
   const store = ctx.cfg.storage;
 
   if (!store) {
     return throwError(() => new Error('no storage service selected'));
   } else {
-    const params$ = ctx.source.hook('before-create', ctx, params).pipe(
-      ensureFilePath(originalPath),
-    );
+    const params$ = ctx.source.hook('before-create', ctx, params);
     return params$.pipe(
       switchMap((params) => store.createFile(params)),
       switchMap((createdFile) => ctx.source.hook('after-create', ctx, createdFile)),
