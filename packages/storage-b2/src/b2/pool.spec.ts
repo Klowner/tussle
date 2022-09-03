9a5568e45ac808f2d3b47489db287f4d4188710f
Mark Riedesel <mark@klowner.com>
2022-09-03 09:44:58 -0400

    storage-b2: update pool tests

--- packages/storage-b2/src/b2/pool.spec.ts
+++ packages/storage-b2/src/b2/pool.spec.ts
@@ -1,39 +1,39 @@
 import { Pool } from './pool';
 
 function incremental(start = 0) {
-  return jest.fn(async () => start++);
+	return jest.fn(async () => ({count: start++}));
 }
 
 describe('b2/pool', () => {
-  describe('acquire', () => {
-    test('calls alloc() when pool is empty', async () => {
-      const alloc = incremental(0);
-      const pool = new Pool(async () => await alloc(), []);
-      const result = await pool.acquire();
-      expect(alloc).toHaveBeenCalled();
-      expect(result).toBe(0);
-    });
+	describe('acquire', () => {
+		test('calls alloc() when pool is empty', async () => {
+			const alloc = incremental(0);
+			const pool = new Pool(async () => await alloc(), []);
+			const result = await pool.acquire();
+			expect(alloc).toHaveBeenCalled();
+			expect(result).toStrictEqual({count: 0});
+		});
 
-    test('returns item from pool when available', async () => {
-      const alloc = incremental(0);
-      const pool = new Pool(async () => (await alloc()).toString(), ['hello']);
-      const result = await pool.acquire();
-      expect(result).toEqual('hello');
-      expect(alloc).not.toHaveBeenCalled();
-    });
-  });
+		test('returns item from pool when available', async () => {
+			const alloc = incremental(0);
+			const pool = new Pool(async () => await alloc(), [{count: 1234}]);
+			const result = await pool.acquire();
+			expect(result).toEqual({count: 1234});
+			expect(alloc).not.toHaveBeenCalled();
+		});
+	});
 
-  describe('release', () => {
-    test('places an item back into the pool for reuse', async () => {
-      const pool = new Pool(
-        async () => 'allocated-string',
-        ['already-in-pool-string'],
-      );
-      pool.release('released-string');
-      expect(pool.toJSON()).toEqual([
-        'already-in-pool-string',
-        'released-string',
-      ]);
-    });
-  });
+	describe('release', () => {
+		test('places an item back into the pool for reuse', async () => {
+			const pool = new Pool(
+				async () => ({value: 'allocated-string'}),
+				[{value: 'already-in-pool-string'}],
+			);
+			pool.release({value: 'released-string'});
+			expect(pool.toJSON()).toEqual([
+				{value: 'already-in-pool-string'},
+				{value: 'released-string'},
+			]);
+		});
+	});
 }); 
