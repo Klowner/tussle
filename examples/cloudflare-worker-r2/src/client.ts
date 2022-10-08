5b2d2b16b6b5a42ad5429960b1ca0cfc4a34a62e
Mark Riedesel <mark@klowner.com>
2022-10-08 10:58:34 -0500

    [cloudflare-worker-r2] fix parallelUploadBoundaries helper to scale down appropriately with smaller files

--- examples/cloudflare-worker-r2/src/client.ts
+++ examples/cloudflare-worker-r2/src/client.ts
@@ -1,33 +1,55 @@
 import { Upload } from 'tus-js-client';
 
+// The parallelUploadBoundaries feature requires tus-js-client 3.0 or newer.
+// While it is not necessary for tussle to function, it is included in this
+// example to demonstrate how to ensure all uploaded chunks within each
+// parallel upload segment are of equal size (excluding the last chunk
+// of the final segment). This is to make the individual R2 records compatible
+// with multipart upload requirements as defined by the S3 API.
 function calculateBoundaries(
 	totalSize: number,
 	chunkSize: number,
-	parallelUploads: number,
-): {start: number; end: number}[] {
+	maxParallelUploads: number,
+): {
+	parallelUploads: number;
+	parallelUploadBoundaries?: {
+		start: number;
+		end: number;
+	}[];
+} {
 	const numChunks = Math.ceil(totalSize / chunkSize);
-	const bytesPerThread = Math.floor(numChunks / parallelUploads) * chunkSize;
-	const ranges = [];
-	for (let i = 0; i < parallelUploads; i++) {
-		ranges[i] = {
-			start: bytesPerThread * i,
-			end: bytesPerThread * (i + 1),
+	const parallelUploads = Math.min(numChunks, maxParallelUploads);
+	if (parallelUploads > 1) {
+		const bytesPerThread = Math.floor(numChunks / parallelUploads) * chunkSize;
+		const ranges = [];
+		for (let i = 0; i < parallelUploads; i++) {
+			ranges[i] = {
+				start: bytesPerThread * i,
+				end: bytesPerThread * (i + 1),
+			};
+		}
+		const remainder = totalSize - (bytesPerThread * parallelUploads);
+		ranges[parallelUploads - 1].end += remainder;
+		return {
+			parallelUploads,
+			parallelUploadBoundaries: ranges,
 		};
 	}
-	const remainder = totalSize - (bytesPerThread * parallelUploads);
-	ranges[parallelUploads - 1].end += remainder;
-	return ranges;
+	return {
+		parallelUploads,
+	};
 }
 
 function uploadFile(file: File) {
 	const chunkSize = 8 * 1024 * 1024;
-	const parallelUploads = 4;
+	const maxParallelUploads = 4;
+	const { parallelUploads, parallelUploadBoundaries } = calculateBoundaries(file.size, chunkSize, maxParallelUploads);
 	const upload = new Upload(file, {
 		endpoint: '/files', // the Cloudflare worker should be running at this URL
 		retryDelays: [0, 1000, 5000],
 		chunkSize,
 		parallelUploads,
-		parallelUploadBoundaries: calculateBoundaries(file.size, chunkSize, parallelUploads),
+		parallelUploadBoundaries,
 		metadata: {
 			filename: file.name,
 			filetype: file.type,
