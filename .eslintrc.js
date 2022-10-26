48e3055191c4d18fcdcaabfe54c8d0dc4488e0b3
Mark Riedesel <mark@klowner.com>
2022-10-26 16:18:23 -0500

    add eslint-plugin-promise

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,26 @@
-const { join } = require('path');
+const {join} = require('path');
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    project: join(__dirname, './packages/tsconfig.settings.json'),
-  },
-  plugins: [
-    'rxjs',
-    'deprecation',
-  ],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-  ],
-  rules: {
-    'semi': 1,
-    'deprecation/deprecation': 'warn',
-    'rxjs/no-async-subscribe': 'error',
-    'rxjs/no-ignored-observable': 'error',
-    'rxjs/no-ignored-subscription': 'error',
-    'rxjs/no-nested-subscribe': 'error',
-  },
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		project: join(__dirname, './packages/tsconfig.settings.json'),
+	},
+	plugins: [
+		'rxjs',
+		'deprecation',
+		'promise',
+	],
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:promise/recommended',
+	],
+	rules: {
+		'semi': 1,
+		'deprecation/deprecation': 'warn',
+		'rxjs/no-async-subscribe': 'error',
+		'rxjs/no-ignored-observable': 'error',
+		'rxjs/no-ignored-subscription': 'error',
+		'rxjs/no-nested-subscribe': 'error',
+	},
 };
