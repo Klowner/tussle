c1b26c108d4cf79e376f6d05557c180a088613cd
Mark Riedesel <mark@klowner.com>
2022-11-02 15:31:40 -0500

    split test environments node/miniflare, update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,43 @@
+const transformTypescript = {
+	'^.+\\.ts$': [
+		'ts-jest',
+		{
+			diagnostics: {
+				ignoreCodes: ['TS151001'],
+			},
+			tsconfig: './packages/tsconfig.json',
+		},
+	],
+};
+
 module.exports = {
-	testEnvironment: 'node',
+	verbose: true,
+	projects: [
+		{
+			transform: transformTypescript,
+			testEnvironment: 'node',
+			testMatch: [
+				'<rootDir>/packages/core/**/*.spec.ts',
+				'<rootDir>/packages/spec/**/*.spec.ts',
+				'<rootDir>/packages/middleware-koa/**/*.spec.ts',
+				'<rootDir>/packages/request-axios/**/*.spec.ts',
+				'<rootDir>/packages/state-memory/**/*.spec.ts',
+				'<rootDir>/packages/state-memory-ttl/**/*.spec.ts',
+				'<rootDir>/packages/state-postgres/**/*.spec.ts',
+			],
+		},
+		{
+			transform: transformTypescript,
+			testEnvironment: 'miniflare',
+			testMatch: [
+				'<rootDir>/packages/middleware-cloudflareworker/src/**/*.spec.ts',
+				'<rootDir>/packages/request-cloudflareworker/src/**/*.spec.ts',
+				'<rootDir>/packages/state-cloudflareworkerkv/src/**/*.spec.ts',
+				'<rootDir>/packages/storage-r2/src/**/*.spec.ts',
+			],
+		}
+	],
 	collectCoverageFrom: [
 		'packages/*/src/**/*.ts',
 	],
-	testMatch: [
-		"**/*.spec.ts",
-	],
-	transform: {
-		'^.+\\.ts$': [
-			'ts-jest',
-			{
-				diagnostics: {
-					ignoreCodes: ['TS151001'],
-				},
-				tsconfig: './packages/tsconfig.json',
-			},
-		],
-	},
 };
