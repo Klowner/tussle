86d40bc2e367687a6d766ce582a8f95df1ec3edb
Mark Riedesel <mark@klowner.com>
2022-11-02 15:40:32 -0500

    remove @tussle/core imports from spec

--- packages/spec/src/middlewareTests.ts
+++ packages/spec/src/middlewareTests.ts
@@ -1,7 +1,16 @@
-import type {TusProtocolExtension, TussleBaseMiddleware, TussleStorageService} from '@tussle/core';
+import type {TussleHookDef, TussleMiddlewareService} from '@tussle/spec/interface/middleware';
+import type {TussleStorageService} from '@tussle/spec/interface/storage';
+import type {TusProtocolExtension} from '@tussle/spec/interface/tus';
 import {EMPTY, Observable} from 'rxjs';
-import {TussleStorageCreateFileParams, TussleStorageCreateFileResponse, TussleStorageFileInfo, TussleStorageFileInfoParams, TussleStoragePatchFileParams, TussleStoragePatchFileResponse} from '../interface/storage';
-import type {TussleHookDef} from '@tussle/spec/interface/middleware';
+
+import {
+	TussleStorageCreateFileParams,
+	TussleStorageCreateFileResponse,
+	TussleStorageFileInfo,
+	TussleStorageFileInfoParams,
+	TussleStoragePatchFileParams,
+	TussleStoragePatchFileResponse
+} from '../interface/storage';
 
 interface MockStorageState {
 	location: string;
@@ -33,19 +42,48 @@ class TussleMockStorageService implements TussleStorageService {
 	}
 }
 
+export interface GenericRequest {
+	method: 'GET'|'POST'|'PUT'|'PATCH'|'DELETE'|'OPTIONS';
+	url: string;
+	headers?: Record<string, string>;
+	body?: Uint8Array;
+}
+
 export function middlewareTests<
-	T extends TussleBaseMiddleware<R, U>, R, U
+	T extends TussleMiddlewareService<R, U>, R, U
 >(
 	name: string,
-	create: <S extends TussleStorageService>(
-		storage: S,
-		hooks: TussleHookDef<R, U>) => Promise<T>,
+	options: {
+		createMiddleware: <S extends TussleStorageService>(
+			storage: S,
+			hooks: TussleHookDef<R, U>,
+		) => Promise<T>,
+		createRequest: (request: GenericRequest) => R,
+		handleRequest: (instance: T, request: R) => Promise<unknown>,
+	},
 ): void {
 	describe(`${name} - middleware specification tests`, () => {
 		test('instantiation', async () => {
 			const storage = new TussleMockStorageService();
-			const instance = await create(storage, {});
+			const instance = await options.createMiddleware(storage, {});
 			expect(instance).not.toBeUndefined();
+		});
+
+		test('file creation', async () => {
+			const storage = new TussleMockStorageService();
+			const instance = await options.createMiddleware(storage, {
+				'before-create': async (ctx, params) => {
+					console.log('BEFORE CREATE', params);
+					return params;
+				},
+			});
+			const request = options.createRequest({
+				method: 'POST',
+				headers: {},
+				url: 'https://tussle-unit-test/foo',
+			});
+			const result = await options.handleRequest(instance, request);
+			expect(result).toBeNull();
 		});
 	});
 }
