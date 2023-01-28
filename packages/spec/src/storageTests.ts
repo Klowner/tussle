cfece9cca7377d7f762e5b887f841cebf687a5a2
Mark Riedesel <mark@klowner.com>
2023-01-28 06:16:15 -0600

    spec: fix typo in test

--- packages/spec/src/storageTests.ts
+++ packages/spec/src/storageTests.ts
@@ -4,7 +4,7 @@ export function storageServiceTests<T extends TussleStorageService>(
   name: string,
   create: () => Promise<T>,
 ): void {
-  describe(`${name} - storage service specification conformancess`, () => {
+  describe(`${name} - storage service specification conformance`, () => {
     test('creation', async () => {
       const instance = await create();
       expect(instance).not.toBeUndefined();
