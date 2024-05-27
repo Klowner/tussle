4025f9b906525fc2c8fc8d991019cc639f8931d5
Mark Riedesel <mark@klowner.com>
2024-05-27 11:50:02 -0500

    WIP: add untested storage-pool implementation

---
+++ packages/storage-pool/src/storage.ts
@@ -0,0 +1,231 @@
+import type {TussleStateService} from '@tussle/spec/interface/state';
+import {
+	TussleStorageCreateFileParams,
+	TussleStorageCreateFileResponse,
+	TussleStorageFileInfo,
+	TussleStorageFileInfoParams,
+	TussleStoragePatchFileParams,
+	TussleStoragePatchFileResponse,
+	TussleStorageService,
+} from "@tussle/spec/interface/storage";
+import {TusProtocolExtension} from "@tussle/spec/interface/tus";
+import {EMPTY, Observable, Subject, catchError, concatMap, defaultIfEmpty, filter, from, map, mergeMap, of, take} from "rxjs";
+
+
+export interface TussleStoragePoolOptions {
+	stores: Record<string, TussleStorageService>;
+	// Cache of upload location to storage pool storage key. Does not have to be
+	// durable as the pool will attempt to rebuild state from stores.
+	stateService: TussleStateService<string>;
+	// // Called when creating a new unrecognized upload.
+	// select: () => Promise<string>;
+}
+
+interface StoragePoolHint {
+	storage: TussleStorageService;
+	storageKey: string;
+}
+
+// If a match is found then the keys array is essentially
+// shifted and wrapped so the array starts with `which`.
+// prioritize([1,2,3], 0) => [1,2,3]
+// prioritize([1,2,3], 2) => [2,3,1]
+// prioritize([1,2,3], 3) => [3,1,2]
+function prioritize<T>(
+	keys: Readonly<T[]>,
+	which: T,
+): T[] {
+	const index = keys.indexOf(which);
+	if (index > -1) {
+		return keys.slice(index).concat(keys.slice(0, index));
+	}
+	return [...keys];
+}
+
+export class TussleStoragePoolError extends Error {}
+export type TussleStoragePoolState = string;
+
+export class TussleStoragePool implements TussleStorageService {
+	constructor (readonly options: TussleStoragePoolOptions) {}
+
+	readonly getStorageByKey = map((key: string) => this.options.stores[key]);
+
+	private readonly error = new Subject<TussleStoragePoolError>();
+
+	get error$() {
+		return this.error.asObservable();
+	}
+
+	private setStickyStoragePath<T extends {location: string}>(key: string) {
+		return concatMap(async (created: T): Promise<T> => {
+			await this.options.stateService.setItem(created.location, key);
+			return {...created, storageKey: key};
+		});
+	}
+
+	// This will override `storageKey` in params if a matching location is found.
+	private getStickyStoragePath<T extends {location: string; storageKey?: string}>() {
+		return concatMap(async (params: T): Promise<T & {storageKey?: string}> => {
+			const storageKey = await this.options.stateService.getItem(params.location) || params.storageKey;
+			return {...params, storageKey};
+		});
+	}
+
+	ensureStickyStoragePath<T extends {location: string} & Partial<StoragePoolHint>>() {
+		return concatMap((params: T) => {
+			if (params.storage || params.storageKey) {
+				return of(params);
+			}
+			return this.getFileInfo(params).pipe(
+				map(({storageKey}) => ({...params, storageKey})),
+				this.setStickyStoragePath(params.location),
+			);
+		});
+	}
+
+	// List all distinct extensions required by the stores within the pool
+	get extensionsRequired(): TusProtocolExtension[] {
+		const distinct = new Set<TusProtocolExtension>();
+		for (const storage of Object.values(this.options.stores)) {
+			storage.extensionsRequired?.forEach(e => distinct.add(e));
+		}
+		return Array.from(distinct.values());
+	}
+
+	// List extensions which are supported by ALL stores within the pool
+	get extensionsSupported(): TusProtocolExtension[] {
+		const stores = Object.values(this.options.stores);
+		const supported = stores.pop()?.extensionsSupported || [];
+		for (const store of stores) {
+			(store.extensionsSupported || []).forEach(ext => {
+				if (!supported.includes(ext)) {
+					supported.splice(supported.indexOf(ext), 1);
+				}
+			});
+		}
+		return supported;
+	}
+
+	// Attempt to create a new upload. If storageKey is provided, that
+	// storage will be prioritized first, but if it fails, creation
+	// may occur in a different storage.
+	//
+	// Successful creation sets the sticky storage path for this upload
+	// to always point to the same storage. Subsequent operations
+	// relating to any given upload need to check the sticky storage
+	// path and direct operations to the same store within the pool.
+	createFile(
+		params: TussleStorageCreateFileParams & Partial<StoragePoolHint>,
+	): Observable<TussleStorageCreateFileResponse & Partial<StoragePoolHint>> {
+		return this.getStores(params).pipe(
+			concatMap(({ storage, storageKey }) => of(storage).pipe(
+				mergeMap(storage => storage.createFile(params)),
+				filter((creation) => creation.success === true),
+				map((creation) => ({...creation, storageKey})),
+				this.setStickyStoragePath(storageKey),
+				catchError((err) => {
+					this.error.next(toTussleError(err));
+					return EMPTY;
+				}), // TODO maybe start avoiding this storage for a moment?
+			)),
+			take(1),
+			defaultIfEmpty({
+				location: params.path,
+				offset: 0,
+				success: false,
+				error: new Error("Exhausted storage pool while attempting to create file"),
+			}),
+		);
+	}
+
+	// Scan each storage in search of the requested file. Optionally provide
+	// `storage` or `storageKey` to accelerate the lookup since it avoids
+	// potentially scanning incorrect stores for the requested file.
+	getFileInfo(
+		params: Readonly<TussleStorageFileInfoParams & Partial<StoragePoolHint>>,
+	): Observable<TussleStorageFileInfo & Partial<StoragePoolHint>> {
+		return of(params).pipe(
+			this.getStickyStoragePath(),
+			concatMap(params => this.getStores(params)),
+			concatMap(({storage, storageKey}) => storage.getFileInfo(params).pipe(
+				filter((fileinfo) => fileinfo.info !== null),
+				map((info) => ({...info, storageKey, storage})),
+				catchError((err: unknown) => {
+					this.error.next(toTussleError(err));
+					return EMPTY;
+				}),
+			)),
+			take(1),
+			defaultIfEmpty({
+				location: params.location,
+				info: null,
+			}),
+		);
+	}
+
+	// Since multi-store reassembly is not supported (PRs welcome), patch
+	// operations can only be dispatched to uploads which have already been
+	// created in a given store.
+	//
+	// Sticky storage paths can potentially be uninitialized in a freshly
+	// spawned worker unless a durable state provider is used.
+	patchFile<Req, U>(
+		params: TussleStoragePatchFileParams<Req, U> & Partial<StoragePoolHint>,
+	): Observable<TussleStoragePatchFileResponse> {
+		return of(params).pipe(
+			this.ensureStickyStoragePath(),
+			concatMap((params) => this.getStores(params).pipe(
+				take(1), // Fail if unable to patch to the selected store.
+				concatMap(({ storage, storageKey }) => storage.patchFile(params).pipe(
+					map((patched) => ({...patched, storage, storageKey})),
+				)),
+			)),
+			take(1),
+			defaultIfEmpty({
+				location: params.location,
+				success: false,
+				complete: false,
+			}),
+		);
+	}
+
+	private getStores<T extends Partial<StoragePoolHint>>(
+		params: Readonly<T>,
+	): Observable<StoragePoolHint> {
+		if (params.storage) {
+			const stores = this.options.stores;
+			const storageKey = Object.keys(stores).find(key => stores[key] === params.storage);
+			if (storageKey) {
+				return this.getPrioritizedStores(storageKey);
+			}
+		}
+		if (params.storageKey) {
+			return this.getPrioritizedStores(params.storageKey);
+		}
+		return this.getPrioritizedStores();
+	}
+
+	private getPrioritizedStores(
+		which?: string, // defaults to first in list if not specified
+	): Observable<{
+		storageKey: string,
+		storage: TussleStorageService,
+	}> {
+		const keys = Object.keys(this.options.stores);
+		which = which || keys[0];
+		return from(prioritize(keys, which)).pipe(
+			map((storageKey) => ({
+				storageKey,
+				storage: this.options.stores[storageKey],
+			})),
+			filter(({ storage }) => !!storage),
+		);
+	}
+}
+
+function toTussleError(err: unknown): Error|TussleStoragePoolError {
+	if (err instanceof Error) {
+		return err;
+	}
+	return new TussleStoragePoolError(typeof err === 'string' ? err : 'unknown storage pool error');
+}
